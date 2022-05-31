f8652a31d6df4d83b5e6aec5669c059255633fb3
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-31 15:39:56 +0300

    Add di

--- lib/features/seasons/widgets/seasons_page.dart
+++ lib/features/seasons/widgets/seasons_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mult_love/common/assets/constants.dart';
 import 'package:mult_love/features/main/data/models/serial.dart';
+import 'package:mult_love/features/seasons/di/seasons_scope.dart';
 
 class SeasonsPage extends StatelessWidget {
   const SeasonsPage({
@@ -12,43 +13,46 @@ class SeasonsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(
-            Constants.mediumPadding,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                serial.title,
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              const SizedBox(
-                height: Constants.bigPadding,
-              ),
-              Expanded(
-                child: GridView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                    crossAxisSpacing: Constants.smallPadding,
-                    mainAxisSpacing: Constants.smallPadding,
-                  ),
-                  itemBuilder: (context, index) => ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.grey[400],
-                    ),
-                    onPressed: () {
-                      //TODO: open series
-                    },
-                    child: Text('$index'),
-                  ),
-                  itemCount: 22,
+    return SeasonsScope(
+      serialLink: serial.link,
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(
+              Constants.mediumPadding,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  serial.title,
+                  style: Theme.of(context).textTheme.headline5,
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: Constants.bigPadding,
+                ),
+                Expanded(
+                  child: GridView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: Constants.smallPadding,
+                      mainAxisSpacing: Constants.smallPadding,
+                    ),
+                    itemBuilder: (context, index) => ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[400],
+                      ),
+                      onPressed: () {
+                        //TODO: open series
+                      },
+                      child: Text('$index'),
+                    ),
+                    itemCount: 22,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
