151a1742529ad0cbcd0dbb97ea19fd81d2bf3fe9
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-31 15:49:53 +0300

    Get seasons

--- lib/features/seasons/data/sources/seasons_source.dart
+++ lib/features/seasons/data/sources/seasons_source.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:html/parser.dart';
 import 'package:mult_love/features/seasons/data/models/season.dart';
 
 class SeasonsSource {
@@ -6,8 +7,35 @@ class SeasonsSource {
 
   SeasonsSource(this.dio);
 
-  Future<List<Season>> getSeasons({required String serialLink}) async{
-    //TODO: get seasons
-    return [];
+  Future<List<Season>> getSeasons({
+    required String serialLink,
+  }) async {
+    final firstSeasonLink = '$serialLink/season.php?id=1';
+    final response = await dio.get(firstSeasonLink);
+    final doc = parse(response.data);
+    final seasons = doc.querySelector('#otherSeasons')!.querySelectorAll('a');
+
+    final result = <Season>[];
+
+    for (final season in seasons) {
+      final link = serialLink + season.attributes['href']!;
+      final number = season.querySelector('div')!.innerHtml;
+
+      result.add(
+        Season(
+          link: link,
+          number: number,
+        ),
+      );
+    }
+
+    result.add(
+      Season(
+        link: firstSeasonLink,
+        number: '1',
+      ),
+    );
+
+    return result;
   }
 }
