f43a93aae69b4572117de48cc0a08cfae7c12e2a
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-01 13:43:34 +0300

    Add di

--- lib/features/specific_series/data/sources/specific_series_source.dart
+++ lib/features/specific_series/data/sources/specific_series_source.dart
@@ -1,6 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:mult_love/features/specific_series/data/models/specific_series.dart';
 
 class SpecificSeriesSource {
+  final Dio dio;
+
+  SpecificSeriesSource(this.dio);
+
   Future<SpecificSeries> getSeries({
     required String link,
   }) async {
---
+++ lib/features/specific_series/di/specific_series_scope.dart
@@ -0,0 +1,57 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mult_love/features/series/data/models/series.dart';
+import 'package:mult_love/features/specific_series/bloc/specific_series_bloc/specific_series_bloc.dart';
+import 'package:mult_love/features/specific_series/data/repositories/specific_series_repository.dart';
+import 'package:mult_love/features/specific_series/data/sources/specific_series_source.dart';
+import 'package:provider/provider.dart';
+
+class SpecificSeriesScope extends StatelessWidget {
+  const SpecificSeriesScope({
+    Key? key,
+    required this.child,
+    required this.serialLink,
+    required this.series,
+  }) : super(key: key);
+
+  final Widget child;
+  final String serialLink;
+  final Series series;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(
+          create: (context) => SpecificSeriesSource(
+            context.read<Dio>(),
+          ),
+        ),
+      ],
+      child: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<ISpecificSeriesRepository>(
+            create: (context) => SpecificSeriesRepository(
+              context.read<SpecificSeriesSource>(),
+            ),
+          ),
+        ],
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => SpecificSeriesBloc(
+                specificSeriesRepository: context.read<ISpecificSeriesRepository>(),
+              )..add(
+                  SpecificSeriesEvent.fetch(
+                     link: series.link,
+                  ),
+                ),
+            ),
+          ],
+          child: child,
+        ),
+      ),
+    );
+  }
+}
