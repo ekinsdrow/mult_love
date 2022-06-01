9e475b80d66a87a25fe0270a096cdefbb4a665a0
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-01 18:08:11 +0300

    Play video

--- lib/features/specific_series/widgets/specific_series_page.dart
+++ lib/features/specific_series/widgets/specific_series_page.dart
@@ -1,3 +1,4 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mult_love/common/assets/constants.dart';
@@ -6,6 +7,7 @@ import 'package:mult_love/features/seasons/data/models/season.dart';
 import 'package:mult_love/features/series/data/models/series.dart';
 import 'package:mult_love/features/specific_series/bloc/specific_series_bloc/specific_series_bloc.dart';
 import 'package:mult_love/features/specific_series/di/specific_series_scope.dart';
+import 'package:video_player/video_player.dart';
 
 class SpecificSeriesPage extends StatefulWidget {
   const SpecificSeriesPage({
@@ -86,7 +88,7 @@ class _SpecificSeriesPageState extends State<SpecificSeriesPage> {
                             setState(() {
                               voiceIndex = index;
                             });
-                            
+
                             context.read<SpecificSeriesBloc>().add(
                                   SpecificSeriesEvent.fetch(
                                     series: widget.series,
@@ -106,15 +108,12 @@ class _SpecificSeriesPageState extends State<SpecificSeriesPage> {
                     const SizedBox(
                       height: Constants.bigPadding,
                     ),
-                    //TODO: video
                     AspectRatio(
                       aspectRatio: 16 / 9,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          widget.series.imageUrl,
-                          fit: BoxFit.cover,
-                          width: double.infinity,
+                        child: _Video(
+                          videoLink: s.videoLink,
                         ),
                       ),
                     ),
@@ -125,6 +124,125 @@ class _SpecificSeriesPageState extends State<SpecificSeriesPage> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _Video extends StatefulWidget {
+  const _Video({
+    Key? key,
+    required this.videoLink,
+  }) : super(key: key);
+
+  final String videoLink;
+
+  @override
+  State<_Video> createState() => _VideoState();
+}
+
+class _VideoState extends State<_Video> {
+  late VideoPlayerController _controller;
+
+  Future<void> _initController(
+    String url,
+  ) async {
+    _controller = VideoPlayerController.network(url);
+    await _controller.initialize();
+
+    _controller.play();
+    return;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initController(widget.videoLink),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GestureDetector(
+            onTap: () {
+              if (_controller.value.isPlaying) {
+                _controller.pause();
+              } else {
+                _controller.play();
+              }
+            },
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: VideoPlayer(
+                    _controller,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: Container(
+                    height: 70,
+                    width: MediaQuery.of(context).size.width -
+                        Constants.mediumPadding * 2,
+                    color: Colors.black38,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: Constants.smallPadding,
+                      ),
+                      child: ValueListenableBuilder<VideoPlayerValue>(
+                          valueListenable: _controller,
+                          builder: (context, value, child) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                  splashRadius: 20,
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    value.isPlaying
+                                        ? Icons.pause
+                                        : Icons.play_arrow,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width -
+                                      Constants.mediumPadding * 2 -
+                                      130,
+                                  child: ProgressBar(
+                                    progress: value.position,
+                                    onSeek: (duration) {
+                                      _controller.seekTo(duration);
+                                    },
+                                    total: value.duration,
+                                  ),
+                                ),
+                                IconButton(
+                                  splashRadius: 20,
+                                  onPressed: () {
+                                    //TODO: fullscreen
+                                  },
+                                  icon: const Icon(
+                                    Icons.fullscreen,
+                                  ),
+                                ),
+                              ],
+                            );
+                          }),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
