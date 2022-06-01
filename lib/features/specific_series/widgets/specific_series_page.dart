5dfbfd6e584ea6edd94440af7b3e66de07c0a5fc
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-01 21:27:48 +0300

    full screen 16/9

--- lib/features/specific_series/widgets/specific_series_page.dart
+++ lib/features/specific_series/widgets/specific_series_page.dart
@@ -54,91 +54,90 @@ class _SpecificSeriesPageState extends State<SpecificSeriesPage> {
               error: () => const Center(
                 child: Text('Ошибка при запросе серии'),
               ),
-              success: (s) => SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (!isPlayerFullScreen)
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.only(
-                              left: Constants.mediumPadding,
-                              right: Constants.mediumPadding,
-                              top: Constants.mediumPadding,
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.serial.title,
-                                  style: Theme.of(context).textTheme.headline5,
-                                ),
-                                const SizedBox(
-                                  height: Constants.smallPadding,
-                                ),
-                                Text(
-                                  '${widget.season.number} сезон - ${widget.seriesIndex} серия',
-                                  style: Theme.of(context).textTheme.headline5,
-                                ),
-                                const SizedBox(
-                                  height: Constants.smallPadding,
-                                ),
-                                Text(
-                                  s.title,
-                                  style: Theme.of(context).textTheme.headline5,
-                                ),
-                                const SizedBox(
-                                  height: Constants.bigPadding,
-                                ),
-                              ],
-                            ),
+              success: (s) => Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (!isPlayerFullScreen)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(
+                            left: Constants.mediumPadding,
+                            right: Constants.mediumPadding,
+                            top: Constants.mediumPadding,
                           ),
-                          SizedBox(
-                            height: 40,
-                            child: ListView.separated(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: Constants.mediumPadding,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.serial.title,
+                                style: Theme.of(context).textTheme.headline5,
                               ),
-                              physics: const BouncingScrollPhysics(),
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (context, index) => ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  primary: voiceIndex == index
-                                      ? Theme.of(context)
-                                          .progressIndicatorTheme
-                                          .color
-                                      : Colors.grey,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    voiceIndex = index;
-                                  });
+                              const SizedBox(
+                                height: Constants.smallPadding,
+                              ),
+                              Text(
+                                '${widget.season.number} сезон - ${widget.seriesIndex} серия',
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                              const SizedBox(
+                                height: Constants.smallPadding,
+                              ),
+                              Text(
+                                s.title,
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                              const SizedBox(
+                                height: Constants.bigPadding,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                          child: ListView.separated(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: Constants.mediumPadding,
+                            ),
+                            physics: const BouncingScrollPhysics(),
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) => ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: voiceIndex == index
+                                    ? Theme.of(context)
+                                        .progressIndicatorTheme
+                                        .color
+                                    : Colors.grey,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  voiceIndex = index;
+                                });
 
-                                  context.read<SpecificSeriesBloc>().add(
-                                        SpecificSeriesEvent.fetch(
-                                          series: widget.series,
-                                          serial: widget.serial,
-                                          link: s.voices[index].link,
-                                        ),
-                                      );
-                                },
-                                child: Text(s.voices[index].name),
-                              ),
-                              separatorBuilder: (_, __) => const SizedBox(
-                                width: Constants.smallPadding,
-                              ),
-                              itemCount: s.voices.length,
+                                context.read<SpecificSeriesBloc>().add(
+                                      SpecificSeriesEvent.fetch(
+                                        series: widget.series,
+                                        serial: widget.serial,
+                                        link: s.voices[index].link,
+                                      ),
+                                    );
+                              },
+                              child: Text(s.voices[index].name),
                             ),
+                            separatorBuilder: (_, __) => const SizedBox(
+                              width: Constants.smallPadding,
+                            ),
+                            itemCount: s.voices.length,
                           ),
-                          const SizedBox(
-                            height: Constants.bigPadding,
-                          ),
-                        ],
-                      ),
-                    Container(
+                        ),
+                        const SizedBox(
+                          height: Constants.bigPadding,
+                        ),
+                      ],
+                    ),
+                  Expanded(
+                    child: Container(
                       padding: EdgeInsets.only(
                         left: !isPlayerFullScreen ? Constants.mediumPadding : 0,
                         right:
@@ -191,8 +190,8 @@ class _SpecificSeriesPageState extends State<SpecificSeriesPage> {
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -220,7 +219,6 @@ class _Video extends StatefulWidget {
   State<_Video> createState() => _VideoState();
 }
 
-
 //TODO: fullscreen 16/9
 class _VideoState extends State<_Video> {
   late VideoPlayerController _controller;
@@ -234,6 +232,8 @@ class _VideoState extends State<_Video> {
     if (!init) {
       _controller = VideoPlayerController.network(url);
       await _controller.initialize();
+
+      // _controller.setVolume(100);
 
       _controller.play();
       init = true;
@@ -270,88 +270,112 @@ class _VideoState extends State<_Video> {
       future: _initController(widget.videoLink),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return GestureDetector(
-            onTap: () {
-              if (!_showPanel) {
-                setState(() {
-                  _showPanel = true;
-                });
-
-                _hidePanelTimerStart();
-              } else {
-                if (_controller.value.isPlaying) {
-                  _controller.pause();
-                } else {
-                  _controller.play();
-                }
-              }
-            },
+          return SafeArea(
             child: Stack(
               children: [
-                Positioned.fill(
-                  child: VideoPlayer(
-                    _controller,
-                    key: widget.playerKey,
+                if (widget.isFullScreen)
+                  Positioned.fill(
+                    child: Container(
+                      color: Colors.black,
+                    ),
+                  ),
+                Positioned(
+                  top: widget.isFullScreen
+                      ? (MediaQuery.of(context).size.height -
+                              MediaQuery.of(context).size.width / 16 * 9) /
+                          2
+                      : 0,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width / 16 * 9,
+                  child: GestureDetector(
+                    onTap: () {
+                      if (!_showPanel) {
+                        setState(() {
+                          _showPanel = true;
+                        });
+
+                        _hidePanelTimerStart();
+                      } else {
+                        if (_controller.value.isPlaying) {
+                          _controller.pause();
+                        } else {
+                          _controller.play();
+                        }
+                      }
+                    },
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: VideoPlayer(
+                            _controller,
+                            key: widget.playerKey,
+                          ),
+                        ),
+                        if (_showPanel)
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            child: Container(
+                              height: 70,
+                              width: widget.isFullScreen
+                                  ? MediaQuery.of(context).size.width
+                                  : MediaQuery.of(context).size.width -
+                                      Constants.mediumPadding * 2,
+                              color: Colors.black38,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: Constants.smallPadding,
+                                ),
+                                child: ValueListenableBuilder<VideoPlayerValue>(
+                                  valueListenable: _controller,
+                                  builder: (context, value, child) {
+                                    return Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        IgnorePointer(
+                                          child: IconButton(
+                                            splashRadius: 20,
+                                            onPressed: () {},
+                                            icon: Icon(
+                                              value.isPlaying
+                                                  ? Icons.pause
+                                                  : Icons.play_arrow,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width -
+                                              Constants.mediumPadding * 2 -
+                                              130,
+                                          child: ProgressBar(
+                                            progress: value.position,
+                                            onSeek: (duration) {
+                                              _controller.seekTo(duration);
+                                            },
+                                            total: value.duration,
+                                          ),
+                                        ),
+                                        IconButton(
+                                          splashRadius: 20,
+                                          onPressed: widget.fullscreenCallback,
+                                          icon: const Icon(
+                                            Icons.fullscreen,
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                          )
+                      ],
+                    ),
                   ),
                 ),
-                if (_showPanel)
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    child: Container(
-                      height: 70,
-                      width: widget.isFullScreen
-                          ? MediaQuery.of(context).size.width
-                          : MediaQuery.of(context).size.width -
-                              Constants.mediumPadding * 2,
-                      color: Colors.black38,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: Constants.smallPadding,
-                        ),
-                        child: ValueListenableBuilder<VideoPlayerValue>(
-                          valueListenable: _controller,
-                          builder: (context, value, child) {
-                            return Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                IgnorePointer(
-                                  child: IconButton(
-                                    splashRadius: 20,
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      value.isPlaying
-                                          ? Icons.pause
-                                          : Icons.play_arrow,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width -
-                                      Constants.mediumPadding * 2 -
-                                      130,
-                                  child: ProgressBar(
-                                    progress: value.position,
-                                    onSeek: (duration) {
-                                      _controller.seekTo(duration);
-                                    },
-                                    total: value.duration,
-                                  ),
-                                ),
-                                IconButton(
-                                  splashRadius: 20,
-                                  onPressed: widget.fullscreenCallback,
-                                  icon: const Icon(
-                                    Icons.fullscreen,
-                                  ),
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                  )
               ],
             ),
           );
