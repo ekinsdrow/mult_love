52351d6d161e236a10d99bc54b0299a2573ae2a8
Ivan Kolchev <ekinsdrow@gmail.com>
2022-10-04 20:45:22 +0300

    fix: update colors

--- lib/common/assets/themes/themes.dart
+++ lib/common/assets/themes/themes.dart
@@ -4,6 +4,9 @@ abstract class Themes {
   static get darkTheme => ThemeData(
         fontFamily: 'Montserrat',
         brightness: Brightness.dark,
+        progressIndicatorTheme: ProgressIndicatorThemeData(
+          color: ThemeData.dark().primaryColor,
+        ),
       );
 }
 
